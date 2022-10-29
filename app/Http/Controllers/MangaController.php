c54832ad8cdbd31538943b3eba0af6469182afa9
Korosive <eddoman33433@gmail.com>
2022-10-29 15:54:20 +1100

    Added search database page

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Manga;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class MangaController extends Controller
 {
@@ -14,16 +16,17 @@ class MangaController extends Controller
     public function index()
     {
         //Fetch all manga from database
-        $mangas = Manga::all();
-        return view('manga.list', ['mangas' => $mangas]);
+        $mangas = DB::table('mangas')->get();
+        return view('welcome', ['mangas' => $mangas]);
     }
 
     /*
         Display MAL Database
     */
-    public function showDatabase()
+    public function search(Request $request)
     {
-        //
+        $response = Http::get('https://api.jikan.moe/v4/manga?&limit=10&page=1');
+        return view('search', ['mangas' => json_decode($response->body())->data]);
     }
 
     /*
